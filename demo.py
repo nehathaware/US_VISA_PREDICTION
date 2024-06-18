4aeacf590b65312622117843cd6a922cf2740814
nehathaware <neha21thaware@gmail.com>
2024-06-18 21:47:46 +0530

    flowcharts added

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ from us_visa.logger import logging
 from us_visa.exception import USvisaException
 import sys
 
-from us_visa.pipline.training_pipeline import TrainPipeline
+from us_visa.pipeline.training_pipeline import TrainPipeline
 
 pipeline = TrainPipeline()
 pipeline.run_pipeline()
