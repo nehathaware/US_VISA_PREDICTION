3ed637175cd2c93cf4493bb33a34088a1193e033
nehathaware <neha21thaware@gmail.com>
2024-07-25 19:15:56 +0530

    Added model trainer pipeline

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -3,6 +3,8 @@ from us_visa.constant import *
 from dataclasses import dataclass
 from datetime import datetime
 
+from us_visa.constant import MODEL_TRAINER_DIR_NAME, MODEL_TRAINER_EXPECTED_SCORE, MODEL_TRAINER_MODEL_CONFIG_FILE_PATH, MODEL_TRAINER_TRAINED_MODEL_DIR
+
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
 @dataclass
@@ -41,4 +43,12 @@ class DataTransformationConfig:
     transformed_object_file_path: str = os.path.join(data_transformation_dir,
                                                      DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
                                                      PREPROCSSING_OBJECT_FILE_NAME)
+    
+
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_dir: str = os.path.join(training_pipeline_config.artifact_dir, MODEL_TRAINER_DIR_NAME)
+    trained_model_file_path: str = os.path.join(model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR, MODEL_FILE_NAME)
+    expected_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
+    model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
 
