433ce53f709650abaac8c792f36ade26138374e2
nehathaware <neha21thaware@gmail.com>
2024-08-02 21:53:04 +0530

    Added model to s3 bucket

--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -8,10 +8,21 @@ from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
 from us_visa.components.model_trainer import ModelTrainer
 from us_visa.components.model_evaluation import ModelEvaluation
+from us_visa.components.model_pusher import ModelPusher
 
-from us_visa.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig)
+from us_visa.entity.config_entity import (DataIngestionConfig, 
+                                          DataValidationConfig, 
+                                          DataTransformationConfig, 
+                                          ModelTrainerConfig, 
+                                          ModelEvaluationConfig, 
+                                          ModelPusherConfig)
 
-from us_visa.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact, ModelEvaluationArtifact)
+from us_visa.entity.artifact_entity import (DataIngestionArtifact, 
+                                            DataValidationArtifact, 
+                                            DataTransformationArtifact, 
+                                            ModelTrainerArtifact, 
+                                            ModelEvaluationArtifact, 
+                                            ModelPusherArtifact)
 
 
 class TrainPipeline:
@@ -21,6 +32,7 @@ class TrainPipeline:
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
         self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -108,6 +120,20 @@ class TrainPipeline:
             raise USvisaException(e, sys)
         
 
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+        
+
         
 
     def run_pipeline(self, ) -> None:
@@ -125,6 +151,8 @@ class TrainPipeline:
             if not model_evaluation_artifact.is_model_accepted:
                 logging.info(f"Model not accepted.")
                 return None
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
+
 
         except Exception as e:
             raise USvisaException(e, sys)
